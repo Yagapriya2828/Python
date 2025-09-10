bee010218a500a344c2c6baf370f3376e13dba8f
R.Yaga priya <rayuduyagapriya@gmail.com>
2025-09-10 11:51:56 +0530

    Mini mistake

--- Weight_Conversion.py
+++ Weight_Conversion.py
@@ -5,10 +5,12 @@ unit = input("Kilograms(K) or Pounds(L): ")
 if unit == "K":
     weight = round(weight * 2.205)
     unit = "Lbs"
+    print(f"Your weight is: {weight} {unit}")
 elif unit == "L":
     weight = round(weight * 0.45)
     unit = "Kgs"
+    print(f"Your weight is: {weight} {unit}")
 else:
     print("Enter a valid unit")
 
-print(f"Your weight is: {weight} {unit}")
+
