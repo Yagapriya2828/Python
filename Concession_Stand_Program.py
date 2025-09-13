6d81a576eab984df7fe8202846009be5ddfedca3
R.Yaga priya <rayuduyagapriya@gmail.com>
2025-09-13 19:53:59 +0530

    new mini_proj(concession stand prog)

---
+++ Concession_Stand_Program.py
@@ -0,0 +1,33 @@
+
+menu = {"popcorn": 10.34,
+        "pizza": 2.00,
+        "pretzel": 2.45,
+        "candy": 1.00,
+        "soda":1.75,
+        "water": 1.90}
+
+cart = []
+total = 0
+
+print("---------MENU------------")
+for key,value in menu.items():
+    print(f"{key:10}:${value:.2f}")
+print("-------------------------")
+
+while True:
+    food = input("Select an item (q to quit): ").lower()
+    if food == "q":
+        break
+    elif menu.get(food) is not None:
+        cart.append(food)
+print("----------YOUR ORDER----------")
+for food in cart:
+    total += menu.get(food)
+    print(food, end = " ")
+
+print()
+print(f"Your Total bill is: ${total:.2f}")
+
+
+
+
